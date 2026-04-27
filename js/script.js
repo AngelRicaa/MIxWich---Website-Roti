d3e93a260d60bfe3f65c37fb14b65a34b0c1ae9c
angel.rica@binus.ac.id <rica0441@gmail.com>
2026-04-27 18:50:16 +0700

    add menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-// Toggle class active
+feather.replace();
+
 const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#hamburger-menu").onclick = () => {
+const hamburger = document.querySelector("#hamburger-menu");
+
+hamburger.onclick = (e) => {
+  e.preventDefault();
   navbarNav.classList.toggle("active");
 };
 
-const hamburger = document.querySelector("#hamburger-menu");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+});
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+const menuCards = document.querySelectorAll(".menu-card");
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".filter-btn.active").classList.remove("active");
+    btn.classList.add("active");
+
+    const filter = btn.dataset.filter;
+
+    menuCards.forEach((card) => {
+      if (filter === "all" || card.classList.contains(filter)) {
+        card.classList.remove("hide");
+      } else {
+        card.classList.add("hide");
+      }
+    });
+  });
 });
